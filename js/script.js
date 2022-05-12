c52d9a5903626ca7eb2d8e1a0322475d7746d1a1
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-12 15:38:27 +0530

    Add html and style of the error message div

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const toggleMenuButton = document.querySelectorAll('.toggle-mobile-menu');
 const seeCloseProjectButton = document.querySelectorAll('.see-close-project');
+const contactForm = document.getElementById('contact-form');
+const formErrorDiv = document.getElementById('error-div');
 
 const toggleDisplay = (section) => {
   section.classList.toggle('display-none');
@@ -25,3 +27,29 @@ toggleMenuButton.forEach((item) => {
 seeCloseProjectButton.forEach((item) => {
   item.addEventListener('click', togglePopUp);
 });
+
+// Form validation
+
+// const validationWithMessage = () => {
+//   let message = '';
+//   const email = contactForm.elements['email'].value;
+//   const styles = getComputedStyle(formErrorDiv);
+//   const errorP = document.getElementById('error-message');
+//   if(styles.visibility === 'hidden' && email !== email.toLowerCase()){
+//     formErrorDiv.style.visibility = 'visible';
+//     formErrorDiv.style.width = '301.865px';
+//     message = `Email Has to be in lower case, You passed: ${email}`;
+//   }
+//   errorP.textContent = message;
+//   return message;
+// }
+
+// contactForm.addEventListener('submit', (ev) => {
+//   ev.preventDefault();
+//   const error = validationWithMessage();
+//   if(!error){
+//     formErrorDiv.style.visibility = 'hidden';
+//     contactForm.submit();
+//   }
+// });
+
