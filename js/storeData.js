071594367b491f7bdfa620a342a449f489fa35ab
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-13 16:25:34 +0530

    Fix linters error

--- js/storeData.js
+++ js/storeData.js
@@ -1,33 +1,33 @@
 const inputFields = document.querySelectorAll('.input-fields');
 
 const getLocalDataValue = () => {
-    if (localStorage.getItem('form-data') !== null) {
-        return JSON.parse(localStorage.getItem('form-data'));
-    }
-    return false;
-}
+  if (localStorage.getItem('form-data') !== null) {
+    return JSON.parse(localStorage.getItem('form-data'));
+  }
+  return false;
+};
 
 const localData = getLocalDataValue();
 
 const formDataObj = {
-    name: localData ? localData.name : '',
-    email: localData ? localData.email : '',
-    message: localData ? localData.message : '',
+  name: localData ? localData.name : '',
+  email: localData ? localData.email : '',
+  message: localData ? localData.message : '',
 };
 
 const dataStoreLocal = (ev) => {
-    const target = ev.target;
-    const value = target.value.trim();
-    if (target.name === 'name') {
-        formDataObj.name = value;
-    }
-    if (target.name === 'email') {
-        formDataObj.email = value;
-    }
-    if (target.name === 'message') {
-        formDataObj.message = value;
-    }
-    return localStorage.setItem('form-data', JSON.stringify(formDataObj));
+  const { target } = ev;
+  const value = target.value.trim();
+  if (target.name === 'name') {
+    formDataObj.name = value;
+  }
+  if (target.name === 'email') {
+    formDataObj.email = value;
+  }
+  if (target.name === 'message') {
+    formDataObj.message = value;
+  }
+  return localStorage.setItem('form-data', JSON.stringify(formDataObj));
 };
 
 inputFields.forEach((item) => item.addEventListener('input', dataStoreLocal));
