71adc9514af4c1e223b7d49ca5e18ff2577aa8e1
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-05-13 16:17:42 +0530

    Add logic to store form data in local storage, and auto fill inputs from local storage on page load.

--- js/storeData.js
+++ js/storeData.js
@@ -1,17 +1,37 @@
 const inputFields = document.querySelectorAll('.input-fields');
 
-const inputName = document.getElementById('input-name');
-const inputEmail = document.getElementById('input-email');
-const inputMessage = document.getElementById('input-message');
+const getLocalDataValue = () => {
+    if (localStorage.getItem('form-data') !== null) {
+        return JSON.parse(localStorage.getItem('form-data'));
+    }
+    return false;
+}
+
+const localData = getLocalDataValue();
 
 const formDataObj = {
-  name: '',
-  email: '',
-  message: '',
+    name: localData ? localData.name : '',
+    email: localData ? localData.email : '',
+    message: localData ? localData.message : '',
 };
 
 const dataStoreLocal = (ev) => {
-  console.log(ev);
+    const target = ev.target;
+    const value = target.value.trim();
+    if (target.name === 'name') {
+        formDataObj.name = value;
+    }
+    if (target.name === 'email') {
+        formDataObj.email = value;
+    }
+    if (target.name === 'message') {
+        formDataObj.message = value;
+    }
+    return localStorage.setItem('form-data', JSON.stringify(formDataObj));
 };
 
 inputFields.forEach((item) => item.addEventListener('input', dataStoreLocal));
+
+document.getElementById('input-name').value = formDataObj.name;
+document.getElementById('input-email').value = formDataObj.email;
+document.getElementById('input-message').value = formDataObj.message;
